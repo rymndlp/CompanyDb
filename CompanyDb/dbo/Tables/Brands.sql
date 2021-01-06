1988c8e60b851f50d0d037aecfd320eff527442f
Raymond-Francisco Soriano Lepe <rymndlp@gmail.com>
2021-01-05 23:56:57 -0600

    Adding test column to Brands table

--- CompanyDb/dbo/Tables/Brands.sql
+++ CompanyDb/dbo/Tables/Brands.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Brands] (
     [BrandID]   BIGINT         IDENTITY (1, 1) NOT NULL,
     [BrandName] NVARCHAR (MAX) NULL,
+    [Test] VARCHAR(50) NULL, 
     CONSTRAINT [PK_dbo.Brands] PRIMARY KEY CLUSTERED ([BrandID] ASC)
 );
 
