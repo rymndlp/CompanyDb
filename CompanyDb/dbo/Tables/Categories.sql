cf5c551d673194485cfafe82467c59bed0e71f7a
Raymond-Francisco Soriano Lepe <rymndlp@gmail.com>
2021-01-06 00:12:35 -0600

    Adding test column to Categories table

--- CompanyDb/dbo/Tables/Categories.sql
+++ CompanyDb/dbo/Tables/Categories.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Categories] (
     [CategoryID]   BIGINT         IDENTITY (1, 1) NOT NULL,
     [CategoryName] NVARCHAR (MAX) NULL,
+    [Test]         VARCHAR(50) NULL,
     CONSTRAINT [PK_dbo.Categories] PRIMARY KEY CLUSTERED ([CategoryID] ASC)
 );
 
